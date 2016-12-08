3b974c45f1cc3710eed22b2cf9b1c27a11a59466
Jonathan Guthrie <jono@perfect.org>
2016-12-08 11:13:55 -0500

    update to use version 1.0+ of auth package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-11-29 19:48:59 +0000
+// Date: 2016-12-08 15:57:57 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectTurnstileSQLiteDemo",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-SQLite.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-SQLite.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 0, minor: 5),
     ]
 )
