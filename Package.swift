30658662fe1bcb3efe6145b577aa30eb977623b0
Jonathan Guthrie <jono@perfect.org>
2017-01-13 14:10:02 -0500

    remove logging dep as it’s already there

--- Package.swift
+++ Package.swift
@@ -1,11 +1,10 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-08 15:57:57 +0000
+// Date: 2017-01-12 17:03:14 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectTurnstileSQLiteDemo",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-SQLite.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 0, minor: 5),
     ]
 )
